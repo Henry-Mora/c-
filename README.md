06a997212aa99dc9a5fe7c195606630cbd7fe123
Henry-Mora <henry.mora@mail.escuelaing.edu.co>
2020-04-20 13:53:00 +0000

    se añaden archivos iniciales.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Este es un titulo 
+text
+
+## Este es un subtitulo
+
+mas texto 
+
+### un subsubtitulo
+aqui se encuentra 
---
+++ primer_ejemplo.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+	cout << "la cantidadde argumentos de entrada son:" << argc << endl;
+	return 0;
+}
